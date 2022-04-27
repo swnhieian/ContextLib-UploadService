2355ee4a8610b7e587816baad9a019340fdf4aff
Weinan Shi <swnhieian@126.com>
2022-04-27 19:52:52 +0800

    fix

--- src/file_utils.py
+++ src/file_utils.py
@@ -21,7 +21,7 @@ def get_dex_name_path(userId, name):
 
 def get_dex_path(userId, name, timestamp):
     d = datetime.datetime.fromtimestamp(int(timestamp) / 1000)
-    return os.path.join(get_dex_name_path(userId, name), d.year, d.month, d.day, timestamp)
+    return os.path.join(get_dex_name_path(userId, name), str(d.year), str(d.month), str(d.day), timestamp)
 
 def get_taskList_path(taskListId):
     return os.path.join(DATA_RECORD_ROOT, taskListId)
